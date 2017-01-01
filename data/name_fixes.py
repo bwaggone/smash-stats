5e9b1c2dfef5c5711270c279edf41344b319aeb4
Brandon W <bwaggone@umich.edu>
2017-01-01 15:05:57 -0500

    Got name fixes file working, updated ratings

--- data/name_fixes.py
+++ data/name_fixes.py
@@ -2,25 +2,21 @@ import os
 import fileinput
 import sys
 
-def replaceAll(file_object,searchExp,replaceExp):
-    for line in file_object
-        if searchExp in line:
-            line = line.replace(searchExp,replaceExp)
-        sys.stdout.write(line)
-
-for line in fileinput.input(files, inplace = 1): 
-          print line.replace("foo", "bar"),
-          
 fix_file = open('name_fixes.csv', 'r')
 
 labels = fix_file.readline()
 
 for line in fix_file:
     fix_info = line.replace('\n', '').split(',')
+    print(fix_info)
     #Open the tournament file associated with the game
     tourneys_filename = './' + fix_info[1] + '/Singles/tournaments.csv'
     tourneys_file = open(tourneys_filename, 'r')
     for line in tourneys_file:
         tourney_info = line.replace('\n', '').split(',')
-        for line in fileinput.input(['get-on-my-level-2016-standings.csv'], inplace = 1):
-            print line.replace("mango", "mang0")
+        if(tourney_info[0] == 'Tournament'):
+            continue
+        for line in fileinput.input(['./' + fix_info[1] + '/Singles/' + tourney_info[0] + '-standings.csv'], inplace = 1):
+            sys.stdout.write(line.replace(fix_info[0] + ',', fix_info[2] + ','))
+        for line in fileinput.input(['./' + fix_info[1] + '/Singles/' + tourney_info[0] + '-sets.csv'], inplace = 1):
+            sys.stdout.write(line.replace(fix_info[0] + ',', fix_info[2] + ','))
