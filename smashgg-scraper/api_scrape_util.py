a143976160dff03be81078b110a0920a6c3888fe
Brandon W <bwaggone@umich.edu>
2017-01-28 15:21:01 -0500

    Merged single/multi into one file, cleanup time

--- smashgg-scraper/api_scrape_util.py
+++ smashgg-scraper/api_scrape_util.py
@@ -2,10 +2,15 @@ import time
 import requests
 import json
 from collections import *
+import progressbar
 
 
+#Constants defined by smash.gg's API
 smash_games = defaultdict(str, {1: "Melee", 2: "ProjectM", 3: "Smash4", 4: "64", 5: "Brawl", 6: "YOLO"})
 smash_formats = defaultdict(str, {1: "Singles", 2: "Doubles", 5: "Crews"})
+
+
+#API Helpers
 api_prefix = 'https://api.smash.gg/'
 api_entrant_postfix = '?expand[]=entrants'
 api_sets_postfix = '?expand[]=sets'
@@ -40,6 +45,118 @@ def get_tournament_info(slug):
         phase_groups[group["phaseId"]].append(group["id"])
 
     return {'name': tournament_name, 'dates': tournament_dates, 'phases_per_event': event_phases, 'groups_per_phase': phase_groups}
+
+#This function will check if the event is worth recording, the following are criteria that if an event has, will be skipped:
+#   Format is not 1 or 2 (Singles or Doubles)
+#   Event Name Contains any of the following:
+#       Lane Shift
+#       Low Tier
+#       Big Brother
+#       Ladder
+def skip_event(events, event):
+    #This prevents side events from being recorded
+    #   The check says, if the format is Singles/Doubles, but Singles/Doubles isn't in the title of the event, skip it
+    not_found = 1
+    for format_num in smash_formats:
+        if(smash_formats[format_num] in events[event].event_name):
+            not_found = 0
+            break;
+   
+    if(not_found or events[event].game == "YOLO" or events[event].game == "" or events[event].format == "Crews" or ("Lane Shift" in events[event].event_name) or ("Crews" in events[event].event_name) or ("Low Tier" in events[event].event_name) or ("Big Brother" in events[event].event_name) or ("Ladder" in events[event].event_name)):
+        return True
+
+    return False
+
+
+
+def update_master_file(master_filename, slug, tournament_name, tournament_dates, event):
+    try:
+        #If we opened the file successfully, check if this tournament has already been recorded. If so, don't write to the master file.
+        master = open(master_filename)
+        master.close()
+        check = False
+        with open(master_filename, "r") as master:
+            for line in master:
+                if slug in line:
+                    check = True
+                    break;
+
+        if(not check):
+            master = open(master_file, "a")
+            master.write(tournament_name + "," + slug + "," + tournament_dates[0] + "," + tournament_dates[1] + "," + str(len(event.entrants)) + "\n")
+            master.close()
+    except:
+        #If the file didn't exist, we'll create the master file.
+        master = open(master_filename, "a+")
+        master.write("Tournament,slug,startDate,endDate,entrants\n")
+        master.write(tournament_name + "," + slug + "," + tournament_dates[0] + "," + tournament_dates[1] + "," + str(len(event.entrants)) + "\n")
+        master.close()
+
+
+def write_set_data(sets_file, event, supress):
+    num_of_groups = 0
+    if(not supress):
+        bar = progressbar.ProgressBar(maxval=len(event.groups),widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+        bar.start()
+
+    f = open(sets_file, "w")
+    if(event.format == "Doubles"):
+        doubles = 1
+        f.write("T1P1,T1P2,T2P1,P2P2,set winner,T1Score,T2Score\n")
+    else:
+        doubles = 0
+        f.write("P1,P2,set winner,P1Score,P2Score\n")
+    
+    for group in event.groups:
+        num_of_groups += 1
+        if(not supress):
+            bar.update(num_of_groups)
+
+        results = requests.get(api_prefix + 'phase_group/' +  str(group) + api_sets_postfix)
+        result_data = json.loads(results.text)
+        #print("Retrieving sets from group #:" + str(group))
+        for _set in result_data["entities"]["sets"]:
+            p1 = _set["entrant1Id"]
+            p2 = _set["entrant2Id"]
+            p1_score = _set["entrant1Score"]
+            p2_score = _set["entrant2Score"]
+
+            if(p1_score == None):
+                p1_score = -2
+            if(p2_score == None):
+                p2_score = -2
+
+            if(p1 == None or p2 == None):
+                continue
+            result = 0
+            if _set["winnerId"] == p2:
+                result = 1
+
+            try:
+                f.write(event.entrants[p1] + ',' + event.entrants[p2] + ',' + str(result) + "," + str(p1_score) + "," + str(p2_score) + '\n')
+            except:
+                f.write((event.entrants[p1] + ',' + event.entrants[p2] + ',' + str(result) + "," + str(p1_score) + "," + str(p2_score) +'\n').encode('utf-8'))
+    if(not supress):
+        bar.finish()
+    #print("Wrote Results to " + filename)
+    f.close()
+    return doubles
+
+
+def write_placements(standings_file, event, doubles):
+    f = open(standings_file, "w")
+    if(doubles):
+        f.write("p1,p2,finalPlacement\n")
+    else:
+        f.write("name,finalPlacement\n")
+    
+    for placing in event.placings:
+        try:
+            f.write(split_doubles_names(event.entrants[placing], doubles) + "," + str(event.placings[placing]) + "\n")
+        except:
+            f.write((split_doubles_names(event.entrants[placing], doubles) + "," + str(event.placings[placing]) + "\n").encode('utf-8'))
+
+    f.close()
 
 
 
--- smashgg-scraper/get_data.py
+++ smashgg-scraper/get_data.py
@@ -1,131 +1,116 @@
 import requests
+import csv
 import time
 import json
 from collections import *
 from api_scrape_util import *
-import progressbar
+import threading
+import sys
 
-slug = raw_input("What is the tournament slug?\n")
+def get_data(slug, supress_output):
+    tourn_info = get_tournament_info(slug)
+    event_phases = tourn_info['phases_per_event']
+    phase_groups = tourn_info['groups_per_phase']
 
-tourn_info = get_tournament_info(slug)
-tournament_name = tourn_info['name']
-tournament_dates = tourn_info['dates']
-event_phases = tourn_info['phases_per_event']
-phase_groups = tourn_info['groups_per_phase']
+    #Separate each phase by game
+    events = {}
+    for event_id in event_phases:
+        r = requests.get(api_prefix + 'event/' + str(event_id) + api_entrant_postfix)
+        evnt_data = json.loads(r.text)
+        events[evnt_data["entities"]["event"]["id"]] = Event(event_id, evnt_data["entities"]["event"]["name"], evnt_data["entities"]["event"]["videogameId"], evnt_data["entities"]["event"]["type"])
+        tmp = evnt_data["entities"]["entrants"]
+        events[evnt_data["entities"]["event"]["id"]].add_entrants(tmp)
 
-#Separate each phase by game
-events = {}
-for event_id in event_phases:
-    r = requests.get(api_prefix + 'event/' + str(event_id) + api_entrant_postfix)
-    evnt_data = json.loads(r.text)
-    events[evnt_data["entities"]["event"]["id"]] = Event(event_id, evnt_data["entities"]["event"]["name"], evnt_data["entities"]["event"]["videogameId"], evnt_data["entities"]["event"]["type"])
-    tmp = evnt_data["entities"]["entrants"]
-    events[evnt_data["entities"]["event"]["id"]].add_entrants(tmp)
+    #At this point, we've scrapped all events, phases, and entrants
+    #print("Retrieved events")
 
-print("Retrieved events")
+    for event in events:
+        events[event].add_phases(event_phases[event])
+        for phase in events[event].phases:
+            events[event].add_groups(phase_groups[phase])
+       
+    for event in events:
+        #Uses the skip criteria defined in skip_event to check if we care about this event.
 
-for event in events:
-    events[event].add_phases(event_phases[event])
-    for phase in events[event].phases:
-        events[event].add_groups(phase_groups[phase])
-   
-for event in events:
-    #This prevents side events from being recorded
-    # Ex: Own the House in TBH6
-    not_found = 1
-    for format_num in smash_formats:
-        if(smash_formats[format_num] in events[event].event_name):
-            not_found = 0
-            break;
-   
-    #64, why do you have a "game" called YOLO? pls
-    #Also crews is just a pain in the ass, skipppp
-    if(not_found or events[event].game == "YOLO" or events[event].game == "" or events[event].format == "Crews" or ("Lane Shift" in events[event].event_name) or ("Crews" in events[event].event_name) or ("Low Tier" in events[event].event_name) or ("Big Brother" in events[event].event_name) or ("Ladder" in events[event].event_name)):
-        print("Skipping 1 event")
-        continue
-    
-    master_file = "../data/" + events[event].game + "/" + events[event].format + "/tournaments.csv" 
-    try:
-        master = open(master_file)
-        master.close()
-        check = False
-        with open(master_file, "r") as master:
-            for line in master:
-                if slug in line:
-                    check = True
+        if(skip_event(events, event)):
+            continue
+        
+        #Update the master tournament file
+        master_file = "../data/" + events[event].game + "/" + events[event].format + "/tournaments.csv" 
 
-        if(not check):
-            master = open(master_file, "a")
-            master.write(tourn_info['name'] + "," + slug + "," + tournament_dates[0] + "," + tournament_dates[1] + "," + str(len(events[event].entrants)) + "\n")
-            master.close()
-    except:
-        master = open(master_file, "a+")
-        master.write("Tournament,slug,startDate,endDate,entrants\n")
-        master.write(tourn_info['name'] + "," + slug + "," + tournament_dates[0] + "," + tournament_dates[1] + "," + str(len(events[event].entrants)) + "\n")
-        master.close()
-    
-    filename = "../data/" + events[event].game + "/" + events[event].format + "/" + slug + "-sets.csv" 
-    print("Working on " + filename + "...")
+        master_lock.acquire()
+        update_master_file(master_file,slug, tourn_info['name'], tourn_info['dates'], events[event])
+        master_lock.release()
+
+        
+        #Update the sets file
+        filename = "../data/" + events[event].game + "/" + events[event].format + "/" + slug + "-sets.csv" 
+        if(not supress_output):
+            print("Working on " + filename + "...")
+        doubles = write_set_data(filename, events[event], supress_output)
 
 
-    #####PROGRESS BAR CODE ##########
-    num_of_groups = 0
-    bar = progressbar.ProgressBar(maxval=len(events[event].groups),widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-    bar.start()
-    #################################
-    
-    f = open(filename, "w")
-    if(events[event].format == "Doubles"):
-        doubles = 1
-        f.write("T1P1,T1P2,T2P1,P2P2,set winner,T1Score,T2Score\n")
-    else:
-        doubles = 0
-        f.write("P1,P2,set winner,P1Score,P2Score\n")
-    
-    for group in events[event].groups:
-        num_of_groups += 1
-        bar.update(num_of_groups)
+        
+        #Update the standings file
+        filename = "../data/" + events[event].game + "/" + events[event].format + "/" + slug + "-standings.csv" 
+        write_placements(filename, events[event], doubles)
 
-        results = requests.get(api_prefix + 'phase_group/' +  str(group) + api_sets_postfix)
-        result_data = json.loads(results.text)
-        #print("Retrieving sets from group #:" + str(group))
-        for _set in result_data["entities"]["sets"]:
-            p1 = _set["entrant1Id"]
-            p2 = _set["entrant2Id"]
-            p1_score = _set["entrant1Score"]
-            p2_score = _set["entrant2Score"]
+    if(supress_output):
+        slug_lock.acquire()
+        all_slugs.pop(slug, None)
+        slug_lock.release()
 
-            if(p1_score == None):
-                p1_score = -2
-            if(p2_score == None):
-                p2_score = -2
 
-            if(p1 == None or p2 == None):
-                continue
-            result = 0
-            if _set["winnerId"] == p2:
-                result = 1
+#Declare all needed threads and locks
+threads = []
+all_slugs = {}
+master_lock = threading.RLock()
+slug_lock = threading.RLock()
 
-            try:
-                f.write(events[event].entrants[p1] + ',' + events[event].entrants[p2] + ',' + str(result) + "," + str(p1_score) + "," + str(p2_score) + '\n')
-            except:
-                f.write((events[event].entrants[p1] + ',' + events[event].entrants[p2] + ',' + str(result) + "," + str(p1_score) + "," + str(p2_score) +'\n').encode('utf-8'))
+def Single():
+    slug = raw_input("What is the tournament slug?\n")
+    get_data(slug, False)
 
-    bar.finish()
-    #print("Wrote Results to " + filename)
-    f.close()
 
-    filename = "../data/" + events[event].game + "/" + events[event].format + "/" + slug + "-standings.csv" 
-    f = open(filename, "w")
-    if(events[event].format == "Doubles"):
-        f.write("p1,p2,finalPlacement\n")
-    else:
-        f.write("name,finalPlacement\n")
-    
-    for placing in events[event].placings:
-        try:
-            f.write(split_doubles_names(events[event].entrants[placing], doubles) + "," + str(events[event].placings[placing]) + "\n")
-        except:
-            f.write((split_doubles_names(events[event].entrants[placing], doubles) + "," + str(events[event].placings[placing]) + "\n").encode('utf-8'))
-    f.close()
+def Multi():
+    #Open the slugs file to read all tournaments to scrape
+    slug_file = "../data/slugs.csv"
+    f = open(slug_file,"r")
+    reader = csv.reader(f)
+    slug_list = list(reader)
+    iterations = len(slug_list[1::])
 
+    for i in range(1,iterations + 1):
+        slug = slug_list[i][1]
+        slug_lock.acquire()
+        all_slugs[slug] = slug
+        #print("Starting Tournament: ", slug)
+        slug_lock.release()
+        #Create a thread to grab data, surpress output
+        t = threading.Thread(target=get_data, args=(slug,True))
+        threads.append(t)
+        t.start()
+
+    #Print the remaining threads, and check every half second.
+    while(threading.activeCount() != 1):
+        sys.stdout.write("Threads Remaining: {0}\r".format(threading.activeCount()))
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+    for thread in threads:
+        thread.join()
+
+    print("Error'd files: ", all_slugs)
+
+
+mode = raw_input("Single Mode (s)? Or File Mode (f)?\n")
+
+valid = False
+if(mode == "s"):
+    Single()
+    valid = True
+if(mode == "f"):
+    Multi()
+    valid = True
+if(not valid):
+    print("Please select a valid mode and rerun.")
